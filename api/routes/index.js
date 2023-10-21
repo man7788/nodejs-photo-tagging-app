19758ea286254693ff62c231f3cd9b312f8e42c1
man7788 <jerman1841@gmail.com>
2023-10-22 03:28:25 +0800

    Add routes for targets

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+const coordsController = require("../controllers/coordsController");
+
+router.get("/coords", coordsController.coords);
+router.post("/target", coordsController.create_target);
 
 module.exports = router;
