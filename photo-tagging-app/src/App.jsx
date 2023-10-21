243c59a9979936aec167795a3f93b4690c517827
man7788 <jerman1841@gmail.com>
2023-10-22 04:07:26 +0800

    Add background img and title

--- photo-tagging-app/src/App.jsx
+++ photo-tagging-app/src/App.jsx
@@ -1,7 +1,15 @@
 import './App.css';
 
 function App() {
-  return <>Hello World!</>;
+  return (
+    <div
+      className="App"
+      // onClick={pop ? clickPicture : clickMenu}
+      // style={{ cursor: cursor }}
+      data-testid="App">
+      <div className="title">{"Where're They?"}</div>
+    </div>
+  );
 }
 
 export default App;
