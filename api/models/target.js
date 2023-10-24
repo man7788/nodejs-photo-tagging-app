39ac64efc4e5b9711faffcaa9df3e13e1b07a7b9
man7788 <jerman1841@gmail.com>
2023-10-25 02:22:22 +0800

    Update target model

--- api/models/target.js
+++ api/models/target.js
@@ -3,10 +3,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TargetSchema = new Schema({
-  coordX: { type: String, required: true, maxLength: 20 },
-  coordY: { type: String, required: true, maxLength: 20 },
-  // coordX: { type: Schema.Types.Decimal128, required: true },
-  // coordY: { type: Schema.Types.Decimal128, required: true },
+  name: { type: String, required: true, maxLength: 200 },
+  style: {
+    border: { type: String, required: true, maxLength: 20 },
+    left: { type: String, required: true, maxLength: 20 },
+    top: { type: String, required: true, maxLength: 20 },
+  },
+});
+
+TargetSchema.pre("validate", function (next) {
+  this.style.border === undefined ? (this.style.border = "none") : undefined;
+  next();
 });
 
 // Export model
