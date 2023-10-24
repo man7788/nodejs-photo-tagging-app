a6c1ff8f78225dd02613fef923851eb6461891f6
man7788 <jerman1841@gmail.com>
2023-10-24 09:00:28 +0800

    Add show hitboxes from data

--- photo-tagging-app/src/App.jsx
+++ photo-tagging-app/src/App.jsx
@@ -1,23 +1,33 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import Dropdown from './components/Dropdown';
+import Target from './components/Target';
 
 function App() {
   const [dropDownDisplay, setDropDownDisplay] = useState('none');
   const [dropDownPosition, setDropDownPosition] = useState();
   const [cursor, setCursor] = useState('pointer');
+  const [onTarget, setOnTarget] = useState('');
+  // Fetch from api
+  const targets = ['peter', 'sam', 'eric'];
+  const hitboxes = {
+    peter: {
+      top: '280px',
+      left: '110px',
+    },
+    sam: { top: '360px', left: '230px' },
+    eric: { top: '320px', left: '350px' },
+  };
 
   const showDropDown = (e) => {
-    console.log('click picture');
-
     const position = {
       menu: {
-        top: `${e.nativeEvent.offsetY + 25}px`,
-        left: `${e.nativeEvent.offsetX + 25}px`,
+        top: `${e.pageY + 25}px`,
+        left: `${e.pageX + 25}px`,
       },
       box: {
-        top: `${e.nativeEvent.offsetY - 28}px`,
-        left: `${e.nativeEvent.offsetX - 28}px`,
+        top: `${e.pageY - 28}px`,
+        left: `${e.pageX - 28}px`,
       },
     };
 
@@ -27,11 +37,18 @@ function App() {
   };
 
   const clickMenu = (e) => {
-    console.log('click menu');
-
     e.stopPropagation();
     setDropDownDisplay('none');
     setCursor('pointer');
+  };
+
+  const clickTarget = (e) => {
+    e.stopPropagation();
+    console.log('on target');
+    setOnTarget(e.target.id);
+    console.log(hitboxes[e.target.id]);
+    // clickPicture(e);
+    // setHide('none');
   };
 
   return (
@@ -41,6 +58,17 @@ function App() {
       style={{ cursor: cursor }}
       data-testid="App">
       <div className={styles.title}>{"Where're They?"}</div>
+      {targets.map((target) => {
+        const position = hitboxes[target];
+        return (
+          <Target
+            key={target}
+            name={target}
+            position={position}
+            clickTarget={clickTarget}
+          />
+        );
+      })}
       <Dropdown
         display={dropDownDisplay}
         position={dropDownPosition}
