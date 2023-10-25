dc9347f13239352ee20c9e0d64e36debc27416ea
man7788 <jerman1841@gmail.com>
2023-10-26 02:30:09 +0800

    Add penguin images

--- photo-tagging-app/src/components/album.js
+++
@@ -1,5 +0,0 @@
-// import peter from './peter.jpg';
-// import sam from './sam.jpg';
-// import eric from './eric.jpg';
-
-// export { peter, sam, eric };
---
+++ photo-tagging-app/src/images/Album.jsx
@@ -0,0 +1,7 @@
+import peter from './peter.jpg';
+import sam from './sam.jpg';
+import eric from './eric.jpg';
+
+const Album = [{ ['Peter']: peter }, { ['Sam']: sam }, { ['Eric']: eric }];
+
+export default Album;
