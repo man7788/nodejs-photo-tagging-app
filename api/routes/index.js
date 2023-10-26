069bb79e57ca0536a2bddd542eebe2d638676a27
man7788 <jerman1841@gmail.com>
2023-10-27 03:16:48 +0800

    Add score routes

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const targetsController = require("../controllers/targetsController");
+const scoresController = require("../controllers/scoresController");
 
 router.get("/targets", targetsController.targets);
 router.post("/target/create", targetsController.create_target);
+
+router.get("/highscore", scoresController.scores);
+router.post("/score/create", scoresController.create_score);
 
 module.exports = router;
