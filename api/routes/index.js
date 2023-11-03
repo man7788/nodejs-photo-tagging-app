773c6aba44793fe38b9170ab9bf87954a9517c1b
man7788 <jerman1841@gmail.com>
2023-11-03 09:40:19 +0800

    Add routes for token

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const verifyToken = require("../controllers/verifyToken");
 
 const targetsController = require("../controllers/targetsController");
 const scoresController = require("../controllers/scoresController");
+const usersController = require("../controllers/usersController");
+
+// router.post("/token", usersController.token);
+router.post("/token", usersController.fetch_token);
 
 router.get("/targets", targetsController.targets);
-router.post("/target/create", targetsController.create_target);
+router.post("/target/create", verifyToken, targetsController.create_target);
 
 router.get("/highscore", scoresController.scores);
-router.post("/score/create", scoresController.create_score);
+router.post("/score/create", verifyToken, scoresController.create_score);
 
 module.exports = router;
