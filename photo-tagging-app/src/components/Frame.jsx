7cab7d2eb25956e46eb8d61503ed1c9e4801affd
man7788 <jerman1841@gmail.com>
2023-11-18 03:05:53 +0800

    Add Frame useEffect

--- photo-tagging-app/src/components/Frame.jsx
+++ photo-tagging-app/src/components/Frame.jsx
@@ -1,8 +1,17 @@
 import styles from '../styles/Frame.module.css';
 import Album from '../images/Album';
 import Photo from './Photo';
+import { useState, useEffect } from 'react';
 
-const Frame = ({ iconStyles }) => {
+const Frame = ({ updateIcon }) => {
+  const [iconStyles, setIconStyles] = useState({});
+
+  useEffect(() => {
+    const newStyle = { color: 'grey', filter: 'brightness(50%)' };
+    const newIconStyles = { ...iconStyles, [updateIcon]: newStyle };
+    setIconStyles(newIconStyles);
+  }, [updateIcon]);
+
   return (
     <div
       className={styles.frame}
@@ -10,15 +19,10 @@ const Frame = ({ iconStyles }) => {
         e.stopPropagation();
       }}>
       {Album.map((peguin) => {
-        const name = Object.keys(peguin);
-        const style = iconStyles[name[0].toLowerCase()];
+        const name = Object.keys(peguin)[0];
+        const style = iconStyles[name.toLowerCase()];
         return (
-          <Photo
-            key={name[0]}
-            photo={peguin[name[0]]}
-            peguin={name[0]}
-            style={style}
-          />
+          <Photo key={name} photo={peguin[name]} peguin={name} style={style} />
         );
       })}
     </div>
