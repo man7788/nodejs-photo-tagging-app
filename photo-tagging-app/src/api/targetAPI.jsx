351b4264080958e68b0667b33ddbcd9375e1e694
man7788 <jerman1841@gmail.com>
2023-11-21 08:57:34 +0800

    Add checkTargetAPI

--- photo-tagging-app/src/api/targetAPI.jsx
+++ photo-tagging-app/src/api/targetAPI.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from 'react';
 import apiDomain from './apiDomain';
 
+const api = apiDomain();
+
 const useTargets = () => {
   const [targets, setTargets] = useState();
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
-  const api = apiDomain();
 
   useEffect(() => {
     fetch(`${api}/target/names`, { mode: 'cors' })
@@ -23,4 +24,20 @@ const useTargets = () => {
   return { targets, error, loading };
 };
 
-export default useTargets;
+const checkTargetAPI = async (postData, updateStyle, setTryAgain) => {
+  try {
+    const response = await fetch(`${api}/target/check`, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+    const data = await response.json();
+    data.result ? updateStyle(data.position) : setTryAgain(true);
+  } catch (err) {
+    console.error(err);
+  }
+};
+export { useTargets, checkTargetAPI };
