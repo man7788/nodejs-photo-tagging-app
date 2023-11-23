3fdb1634014af0e0d3fab0141ea8f2e1296adef6
man7788 <jerman1841@gmail.com>
2023-11-24 07:15:41 +0800

    Update mocks

--- photo-tagging-app/src/components/Popup.test.jsx
+++ photo-tagging-app/src/components/Popup.test.jsx
@@ -1,22 +1,63 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Popup from './Popup';
 
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+vi.mock('../api/tokenAPI', () => ({
+  default: vi.fn().mockReturnValue({
+    token: 'token',
+    tokenError: null,
+    tokenLoading: false,
+  }),
+}));
+
+vi.mock('../api/scoreAPI', () => ({
+  submitScoreAPI: vi.fn().mockReturnValue({
+    result: { name: 'John Doe' },
+  }),
+  useHighScore: vi.fn().mockReturnValue({
+    list: [
+      {
+        _id: '653c00138e1ff544d19b3b63',
+        name: 'test4',
+        time: '00:00:03',
+        __v: 0,
+      },
+      {
+        _id: '653bf8a78e1ff544d19b3b42',
+        name: 'test1',
+        time: '00:00:03',
+        __v: 0,
+      },
+      {
+        _id: '653c00b18e1ff544d19b3b74',
+        name: 'bbb',
+        time: '00:00:03',
+        __v: 0,
+      },
+      {
+        _id: '653bfffc8e1ff544d19b3b5d',
+        name: 'test3',
+        time: '00:00:03',
+        __v: 0,
+      },
+      {
+        _id: '653c00a68e1ff544d19b3b6e',
+        name: 'aaa',
+        time: '00:00:04',
+        __v: 0,
+      },
+    ],
+    error: null,
+    loading: false,
+  }),
+}));
+
 describe('Popup', () => {
   it('should popup when gameover with score', async () => {
-    beforeEach(() => {
-      global.fetch.mockReset();
-      vi.resetModules();
-    });
-
-    const fetchedToken = { token: 'token' };
-
-    function createFetchResponse(data) {
-      return { json: () => new Promise((resolve) => resolve(data)) };
-    }
-
-    global.fetch = vi.fn().mockResolvedValue(createFetchResponse(fetchedToken));
-
     const { rerender } = render(<Popup />);
 
     const popup = await screen.findByTestId('popup');
@@ -35,60 +76,7 @@ describe('Popup', () => {
 
   it('should submit highscore with form input', async () => {
     // https://runthatline.com/how-to-mock-fetch-api-with-vitest/
-    beforeEach(() => {
-      global.fetch.mockReset();
-    });
-
     const user = userEvent.setup();
-
-    const fetchedToken = { token: 'token' };
-
-    const scoreObj = {
-      name: 'Foobar',
-      time: '01:01:01',
-    };
-
-    const fetchedResponse = [
-      {
-        _id: '653c00138e1ff544d19b3b63',
-        name: 'test4',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653bf8a78e1ff544d19b3b42',
-        name: 'test1',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653c00b18e1ff544d19b3b74',
-        name: 'bbb',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653bfffc8e1ff544d19b3b5d',
-        name: 'test3',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653c00a68e1ff544d19b3b6e',
-        name: 'aaa',
-        time: '00:00:04',
-        __v: 0,
-      },
-    ];
-
-    function createFetchResponse(data) {
-      return { json: () => new Promise((resolve) => resolve(data)) };
-    }
-    global.fetch = vi
-      .fn()
-      .mockResolvedValueOnce(createFetchResponse(fetchedToken))
-      .mockResolvedValueOnce(createFetchResponse(scoreObj))
-      .mockResolvedValueOnce(createFetchResponse(fetchedResponse));
 
     render(<Popup updatePopup={{ show: true }} score={'01:01:01'} />);
 
@@ -101,77 +89,18 @@ describe('Popup', () => {
 
     await waitFor(async () => await user.click(button));
 
-    expect(fetch).toHaveBeenNthCalledWith(
-      2,
-      `http://localhost:3000/score/create`,
-      {
-        mode: 'cors',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: 'Bearer token',
-        },
-        body: JSON.stringify(scoreObj),
-      },
-    );
+    const popup = await screen.findByTestId('popup');
+    const highscore = await screen.findByTestId('highscore');
+
+    const popupStyle = getComputedStyle(popup);
+    const highscoreStyle = getComputedStyle(highscore);
+
+    expect(popupStyle.display).toBe('none');
+    expect(highscoreStyle.display).toBe('block');
   });
 
   it('should show highscore after form submit', async () => {
-    beforeEach(() => {
-      global.fetch.mockReset();
-      vi.resetModules();
-    });
-
     const user = userEvent.setup();
-
-    const fetchedToken = { token: 'token' };
-
-    const scoreObj = {
-      name: 'Foobar',
-      time: '01:01:01',
-    };
-
-    const fetchedResponse = [
-      {
-        _id: '653c00138e1ff544d19b3b63',
-        name: 'test4',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653bf8a78e1ff544d19b3b42',
-        name: 'test1',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653c00b18e1ff544d19b3b74',
-        name: 'bbb',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653bfffc8e1ff544d19b3b5d',
-        name: 'test3',
-        time: '00:00:03',
-        __v: 0,
-      },
-      {
-        _id: '653c00a68e1ff544d19b3b6e',
-        name: 'aaa',
-        time: '00:00:04',
-        __v: 0,
-      },
-    ];
-
-    function createFetchResponse(data) {
-      return { json: () => new Promise((resolve) => resolve(data)) };
-    }
-    global.fetch = vi
-      .fn()
-      .mockResolvedValueOnce(createFetchResponse(fetchedToken))
-      .mockResolvedValueOnce(createFetchResponse(scoreObj))
-      .mockResolvedValueOnce(createFetchResponse(fetchedResponse));
 
     render(<Popup updatePopup={{ show: true }} />);
 
@@ -184,13 +113,12 @@ describe('Popup', () => {
 
     await waitFor(async () => await user.click(button));
 
-    const tableHeading = await screen.findByRole('heading', {
-      name: /Top 5 Players/i,
-    });
-    const highscore = await screen.findAllByRole('listitem');
+    const highscore = await screen.findByTestId('highscore');
+    const scores = await screen.findAllByRole('listitem');
 
-    expect(fetch).toHaveBeenCalled(0);
-    expect(tableHeading.textContent).toMatch(/Top 5 Players/);
-    expect(highscore).toHaveLength(5);
+    const highscoreStyle = getComputedStyle(highscore);
+
+    expect(highscoreStyle.display).toBe('block');
+    expect(scores).toHaveLength(5);
   });
 });
