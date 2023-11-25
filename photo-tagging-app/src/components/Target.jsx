bce552e7ebb08fa0e80f81e9c90552ef805b3500
man7788 <jerman1841@gmail.com>
2023-11-26 04:50:34 +0800

    Add propTypes

--- photo-tagging-app/src/components/Target.jsx
+++ photo-tagging-app/src/components/Target.jsx
@@ -1,15 +1,26 @@
 import styles from '../styles/Target.module.css';
+import PropTypes from 'prop-types';
 
-const Target = ({ name, position }) => {
+const Target = ({ name, targetStyle }) => {
   return (
     <div
       id={name}
       className={styles[name]}
-      style={position}
+      style={targetStyle}
       onClick={(e) => {
         e.stopPropagation();
       }}
       data-testid="target"></div>
   );
 };
+
+Target.propTypes = {
+  name: PropTypes.string.isRequired,
+  targetStyle: PropTypes.shape({
+    border: PropTypes.string,
+    top: PropTypes.number,
+    left: PropTypes.number,
+  }),
+};
+
 export default Target;
