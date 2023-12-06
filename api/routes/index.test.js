241a13a764e03c1fb6cc4bbf75984d47d6e9f76d
man7788 <jerman1841@gmail.com>
2023-12-06 09:20:36 +0800

    Factor index tests

--- api/routes/index.test.js
+++ api/routes/index.test.js
@@ -1,7 +1,5 @@
 const index = require("./index");
 const Token = require("../models/token");
-const Target = require("../models/target");
-const Score = require("../models/score");
 
 const { connectDB, dropDB, dropCollections } = require("../setuptestdb");
 
@@ -26,7 +24,7 @@ app.use(express.json());
 app.use("/", index);
 
 describe("JWT", () => {
-  it("should repsonse with jwt token", async () => {
+  it.only("should repsonse with jwt token", async () => {
     const token = new Token({
       handler: "penguins",
       token: "test-token",
@@ -40,147 +38,6 @@ describe("JWT", () => {
 
     expect(response.header["content-type"]).toMatch(/application\/json/);
     expect(response.body).toStrictEqual({ token: "test-token" });
-    expect(response.status).toEqual(201);
-  });
-});
-
-describe("Target", () => {
-  it("should get all target names", async () => {
-    await Target.insertMany([
-      {
-        name: "peter",
-        style: {
-          left: "100",
-          top: "100",
-        },
-      },
-      {
-        name: "sam",
-        style: {
-          left: "100",
-          top: "100",
-        },
-      },
-      {
-        name: "eric",
-        style: {
-          left: "100",
-          top: "100",
-        },
-      },
-    ]);
-
-    const response = await request(app).get("/target/names");
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.body).toStrictEqual([
-      { name: "peter" },
-      { name: "sam" },
-      { name: "eric" },
-    ]);
-    expect(response.status).toEqual(200);
-  });
-
-  it("should repsonse with check target result", async () => {
-    await Target.create({
-      name: "peter",
-      style: {
-        left: "110px",
-        top: "280px",
-      },
-    });
-
-    const postData = {
-      selection: "peter",
-      position: { top: 281, left: 115 },
-      range: { topRange: 65, leftRange: 50 },
-    };
-
-    const response = await request(app)
-      .post("/target/check")
-      .send(postData)
-      .set("Content-Type", "application/json");
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.body).toStrictEqual({
-      result: true,
-      position: { top: "280px", left: "110px" },
-    });
-    expect(response.status).toEqual(201);
-  });
-
-  it("should create a new target", async () => {
-    const target = {
-      name: "peter",
-      left: "110px",
-      top: "280px",
-    };
-
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZGl1IiwiaWF0IjoxNjk4OTc0MzI0fQ.75SWzFkblslFGeUHThGlCc1nXJKz-XR2-HIY6i-XmzE";
-    const response = await request(app)
-      .post("/target/create")
-      .set("Content-type", "application/json")
-      .send(target)
-      .set("Authorization", `Bearer ${token}`);
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.body.name).toBe("peter");
-    expect(response.body.style.left).toBe("110px");
-    expect(response.body.style.top).toBe("280px");
-    expect(response.status).toEqual(201);
-  });
-});
-
-describe("Score", () => {
-  it("should repsonse with highscore", async () => {
-    await Score.insertMany([
-      {
-        name: "test1",
-        time: "00:00:03",
-      },
-      {
-        name: "test2",
-        time: "00:00:03",
-      },
-      {
-        name: "test3",
-        time: "00:00:03",
-      },
-      {
-        name: "test4",
-        time: "00:00:03",
-      },
-      {
-        name: "test5",
-        time: "00:00:03",
-      },
-    ]);
-
-    const response = await request(app).get("/highscore");
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.body).toHaveLength(5);
-    expect(response.status).toEqual(200);
-  });
-
-  it("should create a new highscore", async () => {
-    const score = {
-      name: "moron",
-      time: "00:31:31",
-    };
-
-    const token =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiZGl1IiwiaWF0IjoxNjk4OTc0MzI0fQ.75SWzFkblslFGeUHThGlCc1nXJKz-XR2-HIY6i-XmzE";
-    const response = await request(app)
-      .post("/score/create")
-      .set("Content-type", "application/json")
-      .set("Authorization", `Bearer ${token}`)
-      .send(score);
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.body._id).toBeDefined();
-    expect(response.body.name).toBe("moron");
-    expect(response.body.time).toBe("00:31:31");
     expect(response.status).toEqual(201);
   });
 });
