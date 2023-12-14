dfb4d5cd8219f851583b555148dd8618b0363ef7
man7788 <jerman1841@gmail.com>
2023-12-15 02:11:07 +0800

    Rearrange Album list

--- photo-tagging-app/src/images/Album.jsx
+++ photo-tagging-app/src/images/Album.jsx
@@ -2,6 +2,6 @@ import Peter from './peter.jpg';
 import Sam from './sam.jpg';
 import Eric from './eric.jpg';
 
-const Album = [{ Peter }, { Sam }, { Eric }];
+const Album = [{ Eric }, { Peter }, { Sam }];
 
 export default Album;
