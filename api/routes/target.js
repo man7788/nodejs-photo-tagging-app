0d41451c9b5125509b68478977fdfcae276635ab
man7788 <jerman1841@gmail.com>
2023-12-15 02:21:00 +0800

    Remoe route get names

--- api/routes/target.js
+++ api/routes/target.js
@@ -4,9 +4,6 @@ const verifyToken = require("../controllers/verifyToken");
 
 const targetsController = require("../controllers/targetsController");
 
-// Get all target names
-router.get("/names", targetsController.names);
-
 // Check target from user input
 router.post("/check", targetsController.check_target);
 
