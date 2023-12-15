1726ff8df3892dd9192a1d1844e6e1f2b22410bf
man7788 <jerman1841@gmail.com>
2023-12-15 08:26:40 +0800

    Update App tests

--- photo-tagging-app/src/App.test.jsx
+++ photo-tagging-app/src/App.test.jsx
@@ -59,23 +59,76 @@ vi.mock('./components/Popup', () => ({
   },
 }));
 
-describe('start button', () => {
-  it('should start game when clicked', async () => {
+describe('targets', () => {
+  it('should highlight selected targets', async () => {
     const user = userEvent.setup();
 
     render(<App />);
 
-    const startButtons = screen.getAllByRole('button');
+    const targets = screen.getAllByTestId('photo');
+    const images = screen.getAllByRole('img');
 
-    await user.click(startButtons[0]);
+    await user.click(images[0]);
+    const ericStyleSelected = getComputedStyle(targets[0]);
 
-    const targets = await screen.findAllByTestId('target');
+    expect(ericStyleSelected.filter).toBe('brightness(100%)');
+  });
 
-    expect(targets).toHaveLength(3);
+  it('should show game rule when targets selected', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const images = screen.getAllByRole('img');
+
+    await user.click(images[0]);
+
+    const rule = await screen.findByText(
+      /Select all targets to compete for highscore/i,
+    );
+
+    expect(rule).toBeInTheDocument;
+
+    await user.click(images[1]);
+    await user.click(images[2]);
+
+    expect(rule).not.toBeInTheDocument;
   });
 });
 
-describe.only('highscore button', () => {
+describe('start button', () => {
+  it('should not start game when no target selected', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const startButton = screen.getByRole('button', { name: /start/i });
+
+    await user.click(startButton);
+
+    const warning = await screen.findByText(/Select at least one target/i);
+
+    expect(warning).toBeInTheDocument;
+  });
+
+  it('should start game when at least one target selected', async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+
+    const eric = screen.getByRole('img', { name: /eric/i });
+    const startButton = screen.getByRole('button', { name: /start/i });
+
+    await user.click(eric);
+    await user.click(startButton);
+
+    const clock = await screen.findByTestId('clock');
+
+    expect(clock).toBeInTheDocument;
+  });
+});
+
+describe('highscore button', () => {
   it('should show highscore when clicked', async () => {
     const user = userEvent.setup();
 
